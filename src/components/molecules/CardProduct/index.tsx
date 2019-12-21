ca92e79c154eeeadd46bb7e82937751d90977df2
Yernar <era15314@gmail.com>
2019-12-21 20:00:59 +0330

    cardschange

---
+++ src/components/molecules/CardProduct/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+
+export default function CardProduct() {
+    return <div className="CardPro">
+        <img width="300" height="210" border-radius="20"className="imageproduct" src="https://www.u-nego.com/wp-content/uploads/perf3.jpg" alt="альтернативный текст" />
+        <h2 className ="text2">eshe odin text</h2>
+        <br></br>
+        <p className="text1">lorel ipsum mahandi burger kung s doatavkoi</p>
+        
+    </div>
+    
+   
+}
